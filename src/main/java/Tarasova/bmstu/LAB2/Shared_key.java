d868c33cdc665d04637020de572eaec97d24e7b9
AlexsaTasara <Kirin1337@gmail.com>
2019-11-29 23:36:10 +0300

    Scripted auto-commit on change (2019-11-29 23:36:10) by gitwatch.sh

--- src/main/java/Tarasova/bmstu/LAB2/Shared_key.java
+++ src/main/java/Tarasova/bmstu/LAB2/Shared_key.java
@@ -31,7 +31,7 @@ public class Shared_key implements WritableComparable<Shared_key>  {
         flag = Line.charAt(sizeLine - 1);
         //ID
         airport_ID = Line.substring(0, sizeLine - 1);
-    } 
+    }
     //Функция получения ключа
     public String getAirportID(){
         return airport_ID;
@@ -48,7 +48,7 @@ public class Shared_key implements WritableComparable<Shared_key>  {
     public int compareTo(Shared_key other_key){
         //Сравнение по строкам ID
         int airport_ID_compare = airport_ID.compareTo(other_key.getAirportID());
-        int otherFlag = other_key.getFlag();
+        int otherFlag = other_key.getFlag(); 
         if (airport_ID_compare == 0){
             return flag - otherFlag;
         }
